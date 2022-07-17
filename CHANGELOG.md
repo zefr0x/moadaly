8facfeabc90207e2b6515d5f8017c368053fce17
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-07-17 09:12:38 +0300

    Bump version 0.1.0-alpha.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.1.0-alpha.1
+- First alpha release.
+- Database related features are not yet implemented, the calculation just works, and the data are in memory while the app is running.
+
--- moadaly/__init__.py
+++ moadaly/__init__.py
@@ -1,6 +1,6 @@
 """A feature-rich Qt6 application to calculate the grade point average and other related stuff for university students."""
 
-__version__ = "0.0.0"
+__version__ = "0.1.0-alpha.1"
 __author__ = "zer0-x"
 __maintainer__ = __author__
 __license__ = "GPL-3.0"
