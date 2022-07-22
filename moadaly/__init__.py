b806946bc44d338d5c38328c93542013a397984a
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-07-22 17:19:38 +0300

    Create setup.py file

--- moadaly/__init__.py
+++ moadaly/__init__.py
@@ -1,7 +1,10 @@
-"""A feature-rich Qt6 application to calculate the grade point average and other related stuff for university students."""
+"""Init file for moadaly module."""
 
 __version__ = "0.1.0-alpha.1"
 __author__ = "zer0-x"
 __maintainer__ = __author__
 __license__ = "GPL-3.0"
-__description__ = "A feature-rich Qt6 application to calculate the grade point average and other related stuff for university students."
+__description__ = (
+    "A feature-rich Qt6 application to calculate the grade point average"
+    + " and other related stuff for university students."
+)
---
+++ setup.py
@@ -0,0 +1,60 @@
+"""Setup file for the library."""
+from setuptools import setup, find_packages
+from pathlib import Path
+
+from moadaly import (
+    __version__,
+    __author__,
+    __maintainer__,
+    __license__,
+    __description__,
+)
+
+
+HERE = Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+URL = "https://github.com/zer0-x/moadaly"
+ISSUES = "https://github.com/zer0-x/moadaly/issues"
+CHANGELOG = "https://github.com/zer0-x/moadaly/blob/main/CHANGELOG.md"
+
+setup(
+    name="moadaly",
+    version=__version__,
+    author=__author__,
+    maintainer=__maintainer__,
+    license=__license__,
+    description=__description__,
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url=URL,
+    project_urls={
+        "Issues": ISSUES,
+        "Changelog": CHANGELOG,
+    },
+    packages=find_packages(),
+    install_requires=["PySide6"],
+    entry_points={"console_scripts": ["moadaly = moadaly.__main__:main.main"]},
+    keywords=[
+        "gpa",
+        "gpa-calculator",
+        "cgpa",
+        "qt6",
+        "pyside6",
+        "gpa-calculation-tool",
+        "gui-application",
+        "linux_desktop",
+        "database-gui",
+    ],
+    classifiers=[
+        "Environment :: X11 Applications :: Qt",
+        "Environment :: X11 Applications :: KDE",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Education",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
